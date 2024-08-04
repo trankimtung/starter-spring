a73550ab5c3fe7ae87c47fd96b361c6ac7f062b6
Tran Kim Tung <contact@trankimtung.com>
2024-08-04 15:39:52 +0800

    test: add example test case

---
+++ src/test/kotlin/com/trankimtung/starter/spring/ApplicationTest.kt
@@ -0,0 +1,12 @@
+package com.trankimtung.starter.spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+}
